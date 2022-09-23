cfd852bddf629d1cafe37181a8077c0e933d02c7
Ojo Ayanfe <Ayanfiscoss@gmail.com>
2022-09-22 20:23:34 -0700

    system argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
+if __name == '__main__':
+    import sys
 
-arg = sys.argv
-length = len(arg) - 1
+    arg = sys.argv
+    length = len(arg) - 1
 
-if length == 0:
-    print("0 arguments")
-elif length == 1:
-    print("{} argument:".format(length))
-    print("{}: {}".format(length, arg[length]))
-elif length > 1:
-    print("{} arguments:".format(length))
-    for i in range(length):
-        print("{}: {}".format(i + 1, arg[i + 1]))
+    if length == 0:
+        print("0 arguments")
+    elif length == 1:
+        print("{} argument:".format(length))
+        print("{}: {}".format(length, arg[length]))
+    elif length > 1:
+        print("{} arguments:".format(length))
+        for i in range(length):
+            print("{}: {}".format(i + 1, arg[i + 1]))
