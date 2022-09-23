02e0f3f5e00630c363e6077a9e3e8edf5df3c4c5
Ojo Ayanfe <Ayanfiscoss@gmail.com>
2022-09-22 21:19:37 -0700

    addition of numbers

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+
+    arg = sys.argv
+    length = len(arg) - 1
+    sum = 0
+
+    for i in range(1, length + 1):
+        sum = sum + int(arg[i])
+print("{}".format(sum))
+        
